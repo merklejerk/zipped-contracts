558b9f6c905628efa30be626eacc8eaeb484192c
Lawrence Forman <me@merklejerk.com>
2023-05-23 13:05:00 -0400

    tweaks

--- script/DeployBase.sol
+++ script/DeployBase.sol
@@ -5,12 +5,11 @@ import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import "src/LibZRuntime.sol";
 import "./ZipUtil.sol";
-import "./LibAddresses.sol";
 
 contract DeployBase is Script, ZipUtil {
     function _zcallDeploy(bytes memory unzippedInitCode) internal returns (address deployed) {
         deployed = LibZRuntime.deploySelfExtractingZCallInitCode(
-            LibAddresses.getZ(),
+            _getZ(),
             _zip(unzippedInitCode),
             unzippedInitCode.length,
             bytes8(keccak256(unzippedInitCode))
@@ -20,12 +19,22 @@ contract DeployBase is Script, ZipUtil {
     
     function _zrunDeploy(bytes memory unzippedInitCode) internal returns (address deployed) {
         deployed = LibZRuntime.deploySelfExtractingZRunInitCode(
-            LibAddresses.getZ(),
+            _getZ(),
             _zip(unzippedInitCode),
             unzippedInitCode.length,
             bytes8(keccak256(unzippedInitCode))
         );
         summarize(deployed, unzippedInitCode.length);
+    }
+
+    function _getZ() internal view returns (Z z) {
+        if (block.chainid == 1) {
+            z = Z(0x0000000000000000000000000000000000000000);
+        } else if (block.chainid == 11155111) {
+            z = Z(0x551F0E213dcb71f676558D8B0AB559d1cDD103F2);
+        } else {
+            z = Z(0x3198E681FB81462aeB42DD15b0C7BBe51D38750f);
+        }
     }
 
     function summarize(address deployed, uint256 unzippedSize) private view {
